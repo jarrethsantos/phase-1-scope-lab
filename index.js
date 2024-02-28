9231641b89546d6636fe231268aa32b6788388de
Jarreth Santos <santos.jarreth@gmail.com>
2024-02-28 13:39:59 -0800

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Write your solution in this file!
+var customerName = 'bob';
+ 
+function upperCaseCustomerName() {
+    customerName = 'BOB';
+}
+
+    function setBestCustomer() {
+        bestCustomer = 'not bob';
+    }
+
+    function overwriteBestCustomer() {
+        return bestCustomer = 'maybe bob'
+    }
+
+
+    const leastFavoriteCustomer = 'billy';
+
+    function changeLeastFavoriteCustomer() {
+        return leastFavoriteCustomer = 'jane';
+    }
+
